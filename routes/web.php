ee080d4929be98ace28f65d500b83d1fba0f3515
fauzihsan <muhammadihsan10.mifr@gmail.com>
2021-03-03 13:29:34 +0700

    pertemuan4

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,14 +15,16 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('tryblade.child');
 });
 
-Route::get('/user/{name?}', function ($name = null){
-    return "hai ".$name;
-});
+// Route::resources('/post', [PostController::class, 'index']);
 
-Route::view('/hello', 'hello', ['name' => 'Layung']);
+// Route::get('/user/{name?}', function ($name = null){
+//     return "hai ".$name;
+// });
+
+// Route::view('/hello', 'hello', ['name' => 'Layung']);
 
 // Route::view('/hello', 'hello'); //persingkat
 
